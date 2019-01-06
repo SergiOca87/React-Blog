18fe4de1b6865fe027db5ed6b8a768ed4902c7cf
Sergi <ocasergi@gmail.com>
2019-01-06 23:44:38 +0100

    Add files via upload

---
+++ precache-manifest.c6b98390320200e15607974d403c7798.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9b0767826ecc2eb58e7552962d176f6",
+    "url": "/static/media/typewriter1.c9b07678.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "72d502f7374aa4e913a2",
+    "url": "/static/js/main.72d502f7.chunk.js"
+  },
+  {
+    "revision": "19aa4c47db36f0a24ef3",
+    "url": "/static/js/1.19aa4c47.chunk.js"
+  },
+  {
+    "revision": "72d502f7374aa4e913a2",
+    "url": "/static/css/main.1df67cd5.chunk.css"
+  },
+  {
+    "revision": "1679763cfeab780196d8e6ff296d6e4c",
+    "url": "/index.html"
+  }
+];
